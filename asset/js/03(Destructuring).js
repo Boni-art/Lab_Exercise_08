bd5d1e7d807e51515e989a69d868f3f61fd59e21
Biniyam <se.biniyam.abiy@gmail.com>
2021-02-18 10:38:03 +0300

    all tasks in all the four functions in destructuring.js done

--- asset/js/03(Destructuring).js
+++ asset/js/03(Destructuring).js
@@ -14,21 +14,23 @@ document.addEventListener('DOMContentLoaded', () => {
 function arrayDestructuring() {
 
     // we have an array with the name and surname
-    let arr = ["Margaery", "Tyrell"]
-
+    let arr = ["Margaery", "Tyrell"];
+    let [firstName, lastName] = arr;
     //1. Destructor this array to firstName and lastName
 
     //2. Display it  [Replace it with ${firstName} ${lastName} once you have the variables]
-    des_arr.innerHTML = `Hello , I am firstName lastName `
+    des_arr.innerHTML = `Hello , ${firstName} ${lastName}`;
+    
 
 
     // we have an array with the name and surname
-    let str = "Margaery Tyrell"
+    let str = "Margaery Tyrell";
 
     //1. Destructor this string array to firstName and lastName by using split() method of string object
+    let [firstName2, lastName2] = str.split(' ');
 
     //2. Display it [Replace it with ${firstName} ${lastName} once you have the variables]
-    des_string.innerHTML = `Hello , I am firstName lastName `
+    des_string.innerHTML = `Hello , I am ${firstName2} ${lastName2}`;
 
 
 
@@ -39,18 +41,19 @@ function arrayDestructuring() {
 function objectDestructuring() {
 
     // we have a person  with the fname and surname
-    let person = { firstName: "John", lastName: "Smith" }
+    let person = { firstName: "John", lastName: "Smith" };
 
     //1. Destructor this object to firstName and lastName 
+    let {firstName, lastName} = person;
 
     //2. Display it [Replace it with ${firstName} ${lastName} once you have the variables]
-    des_obj1.innerHTML = `Hi , I am firstName lastName `
+    des_obj1.innerHTML = `Hi , I am ${firstName} ${lastName}`;
 
 
     //1. Destructor this object to fName and sName [different naming]
-
+    let {firstName: fName, lastName: sName} = person;
     //2. Display it [Replace it with ${Name} ${lName} once you have the variables]
-    des_obj2.innerHTML = `Hi , I am fName lName `
+    des_obj2.innerHTML = `Hi , I am ${fName} ${sName}`
 
 
 }
@@ -61,7 +64,7 @@ function spreadSyntax() {
     const arr = [1, 2, 3, 4]
 
     //1. Pass the array using spread syntax
-    const result = sum1();
+    const result = sum1(...arr);
 
     //Display it 
     spread_syntax.innerHTML = `The Sum is : ${result} `
@@ -82,6 +85,7 @@ function restSyntax() {
     const x5 = 1;
 
     //Make the function to support rest so that it can add 5 numbers
+
     const result = sum2(x1, x2, x3, x4, x5);
 
     //Display it 
@@ -101,11 +105,11 @@ function sum1(x1, x2, x3, x4) {
 
 
 //3. Make this function to support rest, and add 5 numbers
-function sum2(x1, x2) {
+function sum2(...args) {
 
     //4. Function that add 5 numbers [Consider Adding the arr[0], arr[1], arr[2]]
 
-    return x1 + x2;
+    return args[0] + args[1] + args[2] + args[3] + args[4];
 
 
 }
