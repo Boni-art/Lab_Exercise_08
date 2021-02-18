5ae1a212002ff13f78bbb5b3e3391a703acb21f4
Biniyam <se.biniyam.abiy@gmail.com>
2021-02-18 10:02:35 +0300

    an id constant that store Symbol with id debugging string created in displaysymbol.

--- asset/js/01(Symbol).js
+++ asset/js/01(Symbol).js
@@ -15,6 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
 function displaySymbol() {
 
     // 1. Create an id constant that store Symbol with id debugging string 
+    const id = Symbol("Debuging_String");
 
     //2. Display the symbol on ui as string using toString()
     symbolResult.innerHTML = "id.toString()";
@@ -24,16 +25,20 @@ function displaySymbol() {
 function displaySymbolEquality() {
 
     // 1. Create an id1 constant that store Symbol with id debugging string 
+    // const id1 = Symbol("Debuging_String");
 
 
     // 2. Create an id2 constant that store Symbol with id debugging string 
+    // const id2 = Symbol("Debuging_String");
 
 
     // 3. Compare their equality (===) , store it on result variable
 
+    let result = id1 === id2;
+
 
     //2. Display the result variable [Remove the string once you have the variable]
-    symbolEquality.innerHTML = "result";
+    symbolEquality.innerHTML = result;
 
 
 
@@ -45,11 +50,13 @@ function displaySymbolObject() {
     const student = { name: "Abebe", age: 21, year: "2dn year" }
 
     // 1. Create an id constant that store Symbol with id debugging string 
+    // const id = Symbol("Debuging_String");
 
 
     //2. Add the symbol property to the object [this will be considered as hidden property]
     //3. Let the Id value be 1234
     // Note : user [] 
+    // student[id] = 1234;
 
 
     //Display the object on console 
